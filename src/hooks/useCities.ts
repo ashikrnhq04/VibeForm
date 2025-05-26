df06a22159e5990e7cc8271944fc0258bce47664
ashikrnhq04 <ashikrn.hq04@gmail.com>
2025-05-26 23:18:25 +0600

    feat: useCities hook to set city based on country

---
+++ src/hooks/useCities.ts
@@ -0,0 +1,39 @@
+import { fetchCities } from "@/features/JobApplicationForm/data";
+import { useEffect, useRef, useState } from "react";
+
+export function useCities(country: string | null) {
+  const [cities, setCities] = useState<{ text: string; value: string }[]>([]);
+  const cache = useRef<Record<string, string[]>>({});
+
+  let unMounted = false;
+
+  useEffect(() => {
+    if (!country) {
+      return;
+    }
+
+    if (cache.current[country]) {
+      setCities(cache.current[country]);
+      return;
+    }
+
+    (async () => {
+      try {
+        const data = await fetchCities(country);
+        if (!unMounted) {
+          setCities(data);
+          cache.current[country] = data;
+        }
+      } catch (error) {
+        console.error("Error fetching cities:", error);
+        return;
+      }
+    })();
+
+    return () => {
+      unMounted = true;
+    };
+  }, [country]);
+
+  return cities;
+}
